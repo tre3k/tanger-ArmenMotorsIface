5dda35a1293e3583f0e1b9fcfbd4f0304ccb8f05
tre3k <pshcyrill@mail.ru>
2018-02-22 12:06:08 +0300

    add motors

--- dialog.cpp
+++ dialog.cpp
@@ -6,9 +6,41 @@ Dialog::Dialog(QWidget *parent) :
     ui(new Ui::Dialog)
 {
     ui->setupUi(this);
+
+    sMotor motor;
+
+    for(int i=0;i<6;i++){
+        motor.path = QString("motors/device/")+QString::number(i);
+        motor.device = new DeviceProxy(QString(motor.path).toStdString().c_str());
+        motors.append(motor);
+        qDebug() << motors.at(i).path << "ping: " << motors.at(i).device->ping() << " s";
+    }
+
+    getState();
+
 }
 
 Dialog::~Dialog()
 {
     delete ui;
+}
+
+void Dialog::getState(){
+    QString state;
+    for(int i=0;i<motors.size();i++){
+        //motors[i].status = QString(motors.at(i).device->status());
+        motors[i].status = QString::fromStdString(motors.at(i).device->status());
+        if(motors.at(i).device->state()==DevState::ON) motors[i].on=true;
+        if(motors.at(i).device->state()==DevState::OFF) motors[i].on=false;
+    }
+
+    ui->listWidget->clear();
+    for(int i=0;i<motors.size();i++){
+        if(motors.at(i).on){
+            state = "ON";
+        }else{
+            state = "OFF";
+        }
+        ui->listWidget->addItem(motors.at(i).path+" "+motors.at(i).status+" "+state);
+    }
 }
--- dialog.h
+++ dialog.h
@@ -2,6 +2,26 @@
 #define DIALOG_H
 
 #include <QDialog>
+#include <QDebug>
+#include <QThread>
+#include <QPushButton>
+#include <QListWidget>
+#include <QVector>
+
+#include <tango.h>
+#include <tango/DeviceProxy.h>
+#include <tango/AttributeProxy.h>
+#include <tango/Connection.h>
+#include <tango/DeviceData.h>
+
+using namespace Tango;
+
+struct sMotor{
+    DeviceProxy *device;
+    QString path;
+    QString status;
+    bool on;
+};
 
 namespace Ui {
 class Dialog;
@@ -17,6 +37,10 @@ public:
 
 private:
     Ui::Dialog *ui;
+
+    QVector<sMotor> motors;
+
+    void getState();
 };
 
 #endif // DIALOG_H
